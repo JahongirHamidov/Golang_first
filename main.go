f9bbe225f30c5255b2f10ba5f5e40390c3fcb886
Jahongir Hamidov <hamidovjahongir444@gmail.com>
2023-07-15 15:49:23 +0500

    git add .

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// func main() {
+// 	fmt.Println("Hello")
+// }
+
+// func main() {
+// 	var name string = "Jakhongir Khamidov"
+// 	var age int = 75
+// 	size, weight := 1.4, false
+
+// 	fmt.Println(age)
+// 	fmt.Printf("%T\n", name, weight)
+// 	fmt.Printf("%T\n", size)
+
+// }
+
+func main() {
+	fmt.Println(math.Floor(2.7))
+	fmt.Println(math.Ceil(2.7))
+	fmt.Println(math.Sqrt(16))
+	// fmt.Println(math.(2.7))
+
+}
